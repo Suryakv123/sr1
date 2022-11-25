7d31269bb7b39cafda3d9a77279dbaa7c3c8759b
Surya <kvsurya55@gmail.com>
2022-11-25 19:51:45 +0530

    commiting

---
+++ core/src/main/java/ui/general/dataaccess/api/dao/BinaryObjectRepository.java
@@ -0,0 +1,11 @@
+package ui.general.dataaccess.api.dao;
+
+import io.oasp.module.jpa.dataaccess.api.data.DefaultRepository;
+import ui.general.dataaccess.api.BinaryObjectEntity;
+
+/**
+ * {@link DefaultRepository} for {@link BinaryObjectEntity}.
+ */
+public interface BinaryObjectRepository extends DefaultRepository<BinaryObjectEntity> {
+
+}
